430f347570580b5d14b6b993b42cd2ca5a225622
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2023-02-27 21:19:38 -0300

    Create 1064.c

---
+++ 1064.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int cont=0,aux;
+	float n,s=0;
+	for(aux=0;aux<6;aux++)
+	{
+		scanf("%f",&n);
+		if(n>0)
+		{
+			s+=n;
+			cont++;
+		}
+	}
+		printf("%i valores positivos\n",cont);
+		printf("%.1f\n",s/cont);
+ return 0;
+}

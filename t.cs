2885921273ab094f433dc1209f6f2fadcf69916d
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2023-03-06 22:06:45 -0300

    Update t.cs

--- t.cs
+++ t.cs
@@ -1,31 +1,16 @@
 using System;
 
-class Program {
-  static void Main(string[] args) {
-    int a, b, c;
-    string[] input = Console.ReadLine().Split();
-    a = int.Parse(input[0]);
-    b = int.Parse(input[1]);
-    c = int.Parse(input[2]);
-    if(a > b)
-    {
-        if(b > c)
-        {
-            if((b - c) < (a - b)) Console.WriteLine(":)");
-            else Console.WriteLine(":(");
-        }
-        else Console.WriteLine(":)");
+class MainClass {
+  public static void Main (string[] args) {
+    string line;
+    long n1, n2, sum;
+
+    while ((line = Console.ReadLine()) != null) {
+      string[] input = line.Split(' ');
+      n1 = long.Parse(input[0]);
+      n2 = long.Parse(input[1]);
+      sum = n1 ^ n2;
+      Console.WriteLine(sum);
     }
-    else if(b > a)
-    {
-        if(c > b)
-        {
-            if((c - b) < (b - a)) Console.WriteLine(":(");
-            else Console.WriteLine(":)");
-        }
-        else Console.WriteLine(":(");
-    }
-    else if(c > b) Console.WriteLine(":)");
-    else Console.WriteLine(":(");
   }
 }

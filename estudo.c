279b4d0a39a29f85ad04a94f7bb91d9fcaf5f3a4
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2023-03-07 21:54:51 -0300

    Update estudo.c

--- estudo.c
+++ estudo.c
@@ -1,38 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+using System;
 
-void escrever(char *f);
-
-int main(void)
+class Program
 {
-    char nome[] = {"Conto.txt"};
-
-    escrever(nome);
-
-    return 0;
-}
-
-void escrever(char *f)
-{
-    FILE *arq = fopen(f,"w");
-
-    char texto[500];
-
-    if(arq)
+    static void Main()
     {
-        printf("DIGITE UMA FRASE OU UM CARACTER PARA FINALIZAR\n");
-        fgets(texto,500,stdin);
+        string a, b, c;
+        a = Console.ReadLine();
+        b = Console.ReadLine();
+        c = Console.ReadLine();
 
-        while(strlen(texto) > 1)//se for == ou != a 1 ele para
-        {
-            fputs(texto, arq);//usada para escrever uma string em um arquivo
-            //fputc('\n',arq);
-            fgets(texto,500,stdin);
-            //scanf("%*c");
-        }
-        fclose(arq);
-    }else{
-        printf("ERROR!\n");
+        if (a[0] == 'v' && b[0] == 'a' && c[0] == 'c') Console.WriteLine("aguia");
+        if (a[0] == 'v' && b[0] == 'a' && c[0] == 'o') Console.WriteLine("pomba");
+        if (a[0] == 'v' && b[0] == 'm' && c[0] == 'o') Console.WriteLine("homem");
+        if (a[0] == 'v' && b[0] == 'm' && c[0] == 'h') Console.WriteLine("vaca");
+        if (a[0] == 'i' && b[0] == 'i' && c[2] == 'm') Console.WriteLine("pulga");
+        if (a[0] == 'i' && b[0] == 'i' && c[2] == 'r') Console.WriteLine("lagarta");
+        if (a[0] == 'i' && b[0] == 'a' && c[0] == 'h') Console.WriteLine("sanguessuga");
+        if (a[0] == 'i' && b[0] == 'a' && c[0] == 'o') Console.WriteLine("minhoca");
+        
     }
 }

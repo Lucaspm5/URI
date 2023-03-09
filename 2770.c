54079cc7d1ff3dbf7b3ccf2f2df78cfbfff3aba3
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2023-03-08 21:04:24 -0300

    Update 2770.c

--- 2770.c
+++ 2770.c
@@ -1,36 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-typedef struct {
-    int x;
-    int y;
-} scss;
 
 int main() {
-    while (1) {
-        char line[1000];
-        fgets(line, sizeof(line), stdin);
-        if (strlen(line) == 0 || strcmp(line, "\n") == 0) {
-            break;
-        }
-        char *token;
-        token = strtok(line, " ");
-        scss board;
-        board.x = atoi(token);
-        token = strtok(NULL, " ");
-        board.y = atoi(token);
-        token = strtok(NULL, " ");
-        int m = atoi(token);
-
+    int x, y, m;
+    while (scanf("%d %d %d", &x, &y, &m) != EOF) {
         for (int i = 0; i < m; i++) {
-            fgets(line, sizeof(line), stdin);
-            token = strtok(line, " ");
-            int xi = atoi(token);
-            token = strtok(NULL, " ");
-            int yi = atoi(token);
-
-            if ((xi <= board.x && yi <= board.y) || (xi <= board.y && yi <= board.x)) {
+            int xi, yi;
+            scanf("%d %d", &xi, &yi);
+            if (xi <= x && yi <= y || xi <= y && yi <= x) {
                 printf("Sim\n");
             } else {
                 printf("Nao\n");
